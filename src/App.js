416ff0be5985afb637a3fc9d259d0c4edaabac56
aimeliala <idstack@outlook.co.id>
2023-07-06 01:00:16 +0700

    mengubah local variabel yg dinamis dengan state

--- src/App.js
+++ src/App.js
@@ -1,21 +1,28 @@
+import { useState } from "react";
+
 const stepItems = ["Dream", "Believe", "Achieve"];
 
 function App() {
-  const step = 1;
+  // const [step, setStep] = React.useState(1);
+  // const arr = useState(1);
+  // console.log(arr);
+  const [step, setStep] = useState(1);
 
   function handlePrev() {
-    alert("prev");
+    if (step > 1) setStep(step - 1);
+    // setStep(step - 1);
   }
   function handleNext() {
-    alert("next");
+    if (step < 3) setStep(step + 1);
+    // setStep(step + 1);
   }
 
   return (
     <div className="steps">
       <div className="numbers">
-        <div className={`${step >= 1 ? "active" : ""}`}>1</div>
-        <div className={`${step >= 2 ? "active" : ""}`}>2</div>
-        <div className={`${step >= 3 ? "active" : ""}`}>3</div>
+        <div className={step >= 1 ? "active" : ""}>1</div>
+        <div className={step >= 2 ? "active" : ""}>2</div>
+        <div className={step >= 3 ? "active" : ""}>3</div>
       </div>
       <p className="message">
         Step {step}: {stepItems[step - 1]}
