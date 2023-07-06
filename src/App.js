4ce54eebc696a1d1103a46385d05acb8af2735ff
aimeliala <idstack@outlook.co.id>
2023-07-06 12:37:59 +0700

    contoh lain penggunakan state untuk render component

--- src/App.js
+++ src/App.js
@@ -7,8 +7,9 @@ function App() {
   // const arr = useState(1);
   // console.log(arr);
   const [step, setStep] = useState(1);
+  const [isOpen, setIsOpen] = useState(true);
   // Bad practice
-  const [test] = useState({ name: "Aim" });
+  // const [test] = useState({ name: "Aim" });
 
   function handlePrev() {
     if (step > 1) setStep(step - 1);
@@ -19,37 +20,44 @@ function App() {
     // setStep(step + 1);
     // step = step + 1;
     // Bad practice
-    test.name = "Lynn";
+    // test.name = "Lynn";
   }
 
   return (
-    <div className="steps">
-      <div className="numbers">
-        <div className={step >= 1 ? "active" : ""}>1</div>
-        <div className={step >= 2 ? "active" : ""}>2</div>
-        <div className={step >= 3 ? "active" : ""}>3</div>
-      </div>
-      <p className="message">
-        Step {step}: {stepItems[step - 1]}
-        {test.name}
-      </p>
-      <div className="buttons">
-        <button
-          style={{ backgroundColor: "#526D82", color: "#fff" }}
-          // onClick={() => alert("Prev")}
-          onClick={handlePrev}
-          // onMouseEnter={() => alert("Prev")}
-        >
-          Prev
-        </button>
-        <button
-          style={{ backgroundColor: "#526D82", color: "#fff" }}
-          onClick={handleNext}
-        >
-          Next
-        </button>
-      </div>
-    </div>
+    <>
+      <button className="close" onClick={() => setIsOpen(!isOpen)}>
+        &times;
+      </button>
+      {isOpen && (
+        <div className="steps">
+          <div className="numbers">
+            <div className={step >= 1 ? "active" : ""}>1</div>
+            <div className={step >= 2 ? "active" : ""}>2</div>
+            <div className={step >= 3 ? "active" : ""}>3</div>
+          </div>
+          <p className="message">
+            Step {step}: {stepItems[step - 1]}
+            {/* {test.name} */}
+          </p>
+          <div className="buttons">
+            <button
+              style={{ backgroundColor: "#526D82", color: "#fff" }}
+              // onClick={() => alert("Prev")}
+              onClick={handlePrev}
+              // onMouseEnter={() => alert("Prev")}
+            >
+              Prev
+            </button>
+            <button
+              style={{ backgroundColor: "#526D82", color: "#fff" }}
+              onClick={handleNext}
+            >
+              Next
+            </button>
+          </div>
+        </div>
+      )}
+    </>
   );
 }
 
